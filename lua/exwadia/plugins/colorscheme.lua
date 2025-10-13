2a1dc03c823c5c7839082781824a41beccad832e
fr-wawan <hermawantan12@gmail.com>
2025-10-13 18:19:11 +0800

    update theme

--- lua/exwadia/plugins/colorscheme.lua
+++ lua/exwadia/plugins/colorscheme.lua
@@ -1,7 +1,15 @@
 return {
-  'catppuccin/nvim',
+  'shaunsingh/nord.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
-    vim.cmd 'colorscheme catppuccin-mocha'
+    vim.g.nord_contrast = true
+    vim.g.nord_borders = false
+    vim.g.nord_disable_background = false
+    vim.g.nord_italic = false
+    vim.g.nord_uniform_diff_background = true
+    vim.g.nord_bold = false
+
+    -- Load the colorscheme
+    require('nord').set()
   end,
 }
